f8f836b8065750b291f80172311cdc7da7ed10c6
prescottbreeden <prescott.breeden@gmail.com>
2018-12-17 22:07:05 -0800

    linked angular to express
    serving dist static and added catch all for routing

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+/* ..................................................................
+ * .                                                                .
+ * .     BUG HUNTER - Coding Dojo Student Resource/Learning App     .
+ * .                                                                .
+ * .             Prescott Breeden & Bryanna Awesome                 .
+ * .                                                                .
+ * ..................................................................
+*/
+
 const express = require('express');
 const app = express();
 
@@ -5,8 +14,14 @@ const bp = require('body-parser');
 app.use(bp.urlencoded({ extended: true }))
 app.use(bp.json());
 
+app.use(express.static( __dirname + '/public/dist/public' ));
+
 const routes = require('./server/routes');
 routes(app);
+
+app.all("*", (req, res) => {
+  res.sendFile(path.resolve("./public/dist/public/index.html"))
+});
 
 const PORT = 8080;
 app.listen(PORT, ()=> {
